29a796fb86e3f884c769c1518dea809b49a6be94
DaoVanTuyen-98 <tuyen0511998@gmail.com>
2021-03-09 20:58:39 +0700

    update upto 09.03.2021

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.slideBanner').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
